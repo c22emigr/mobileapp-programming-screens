62439c044598146c3cdc1277793dc826745732ef
c22emigr <c22emigr@student.his.se>
2023-04-24 14:50:56 +0200

    Finished intent and added stuff to second acitivity so the string and int is shown

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -4,22 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Intent knappen;
+   private Intent knopp;
+   private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.button);
 
-        btn.setOnClickListener();
-        Intent knappen = new Intent(MainActivity.this, MainActivity2.class);
-        intent.putExtra("name", "Daniel"); // Optional
-        intent.putExtra("number", 1); // Optional
-        startActivity(knappen);
+        button1 = findViewById(R.id.button);
+
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent (MainActivity.this, MainActivity2.class));
+                knopp = new Intent(MainActivity.this, MainActivity2.class);
+                knopp.putExtra("name", "Emil"); // Optional
+                knopp.putExtra("number", 99); // Optional
+                startActivity(knopp);
+            }
+    });
     }
-
 }
--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -3,12 +3,26 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    String Name;
+    int Number;
+
+    private TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            Name = extras.getString("name");
+            Number = extras.getInt("number");
+            textview = findViewById(R.id.textView1);
+            textview.setText(Name+Number);
+        }
     }
 }
