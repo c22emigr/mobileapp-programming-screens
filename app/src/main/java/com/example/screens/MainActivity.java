a9ae8ceba28dc53cdea96095c7ede00020c82840
c22emigr <c22emigr@student.his.se>
2023-04-24 14:14:33 +0200

    Added button and intent

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Intent knappen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener();
+        Intent knappen = new Intent(MainActivity.this, MainActivity2.class);
+        intent.putExtra("name", "Daniel"); // Optional
+        intent.putExtra("number", 1); // Optional
+        startActivity(knappen);
     }
+
 }
